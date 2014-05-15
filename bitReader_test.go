09626cfad8ad75cf5eb941cbd26d267cdd4337e9
Ben Eggers <beggerss@cs.washington.edu>
2014-05-15 13:33:38 -0700

    more test progress, but still not working...

--- bitReader_test.go
+++ bitReader_test.go
@@ -6,9 +6,9 @@ package bitIO
 // Tests the BitReader
 
 import (
-	"testing"
 	"io/ioutil"
 	"os"
+	"testing"
 )
 
 // globals
@@ -20,8 +20,9 @@ var filename = ".test"
 
 func TestNewReader(t *testing.T) {
 	err := makeBasicFile()
+	defer os.Remove(filename)
 	if err != nil {
-		t.Error("Couldn't create file: ", err)
+		t.Error(err)
 	}
 	b, err := NewReader(filename)
 	b.Close()
@@ -30,7 +31,7 @@ func TestNewReader(t *testing.T) {
 func TestNewReaderReturnsErrorOnNonExistentFile(t *testing.T) {
 	_, err := NewReader(filename)
 	if err == nil {
-		t.Error("err should have been non-nil but was ", err)
+		t.Error("err should have been non-nil but was nil")
 	}
 }
 
@@ -39,7 +40,35 @@ func TestNewReaderReturnsErrorOnNonExistentFile(t *testing.T) {
 ////////////////////////////////////////////////////////////////////////////////
 
 func TestBasicFileRead(t *testing.T) {
-	makeBasicFile()
+	err := makeBasicFile()
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+	br, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// We should get 7 0's
+	for i := 0; i < 7; i++ {
+		bit, err := br.ReadBit()
+		if err != nil {
+			t.Error(err)
+		}
+		if bit != 0 {
+			t.Error("Got incorrect bit. Should be 0, got", bit, "for bit", i)
+		}
+	}
+
+	// Then a 1
+	bit, err := br.ReadBit()
+	if err != nil {
+		t.Error(err)
+	}
+	if bit != 1 {
+		t.Error("Got incorrect bit. Should be 1, got", bit)
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -49,11 +78,10 @@ func TestBasicFileRead(t *testing.T) {
 // Makes a basic file to be used by the test suite. File bytes will be
 // ascending powers of two.
 func makeBasicFile() (err error) {
-	bytes := make([]byte, 10)
-	for i := 0; i < len(bytes); i++ {
+	bytes := make([]byte, 8)
+	for i := 0; i < len(bytes) - 1; i++ {
 		bytes[i] = 1 << uint8(i)
- 	}
- 	err = ioutil.WriteFile(filename, bytes, 0644)
- 	defer os.Remove(filename)
- 	return err
+	}
+	err = ioutil.WriteFile(filename, bytes, 0644)
+	return err
 }

944917c4207923b3c08129e40a192713b0af54bf
Ben Eggers <beggerss@cs.washington.edu>
2014-05-15 06:10:45 -0700

    initial commit

---
+++ bitReader_test.go
@@ -0,0 +1,12 @@
+// Ben Eggers
+// GNU GPL'd
+
+// Tests the ByteReader
+
+package bitIO
+
+import "testing"
+
+func TestNothingAtAll(t *testing.T) {
+	// do nothing
+}
---
+++ bitWriter_test.go
@@ -0,0 +1,12 @@
+// Ben Eggers
+// GNU GPL'd
+
+// Tests the ByteWriter
+
+package bitIO
+
+import "testing"
+
+func TestNothing(t *testing.T) {
+	// do nothing
+}

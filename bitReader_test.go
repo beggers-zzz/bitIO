eebd9b8a1e8042791d5cf9fd962ee0416e6a95ad
Ben Eggers <beggerss@cs.washington.edu>
2014-05-15 12:59:32 -0700

    started bitReader tests

--- bitReader_test.go
+++ bitReader_test.go
@@ -1,12 +1,48 @@
 // Ben Eggers
 // GNU GPL'd
 
-// Tests the BitReader
-
 package bitIO
 
-import "testing"
+// Tests the BitReader
 
-func TestNothingAtAll(t *testing.T) {
+import (
+	"testing"
+	"io/ioutil"
+	"os"
+)
+
+// globals
+var filename = ".test"
+
+// NewReader tests
+
+func TestNewReader(t *testing.T) {
+	err := makeBasicFile()
+	if err != nil {
+		t.Error("Couldn't create file: ", err)
+	}
+	b, err := NewReader(filename)
+	b.Close()
+}
+
+func TestNewReaderReturnsErrorOnNonExistentFile(t *testing.T) {
+
+}
+
+// ReadBit tests
+
+func TestBasicFileRead(t *testing.T) {
 	// do nothing
+}
+
+// Makes a basic file to be used by the test suite. File bytes will be
+// ascending powers of two.
+func makeBasicFile() (err error) {
+	bytes := make([]byte, 10)
+	for i := uint8(0); i < uint8(len(bytes)); i++ {
+		bytes[i] = 1 << i
+ 	}
+ 	err = ioutil.WriteFile(filename, bytes, 0644)
+ 	defer os.Remove(filename)
+ 	return err
 }

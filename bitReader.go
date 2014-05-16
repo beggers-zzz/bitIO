5e9515abaf22a8c59862edca4ef0a548d49b72db
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 12:18:08 -0700

    got bitreader working (at least, passing a basic test)

--- bitReader.go
+++ bitReader.go
@@ -8,7 +8,6 @@ package bitIO
 
 import (
 	"errors"
-	"fmt"
 )
 
 type BitReader struct {
@@ -18,26 +17,24 @@ type BitReader struct {
 // Set up and return a BitReader on the passed file.
 func NewReader(file string) (b BitReader, err error) {
 	str, err := newStruct(file)
-	return BitReader{str}, err
+	if err != nil {
+		return BitReader{}, err
+	}
+	b = BitReader{str}
+	// We need to initialize it on a byte!
+	err = b.nextByte()
+	return b, err
 }
 
 // Returns the next bit on the file stream. Will always be 0 or 1. Will
 // return a non-nil err iff the read failed, or on EOF
 func (b BitReader) ReadBit() (bit byte, err error) {
-	fmt.Println("bits:", b.Bits[0], "numBits", b.NumBits)
-	bit = b.Bits[0] & (1 << 7) >> 7 // get the highest-order bit
+	bit = (b.Bits[0] & (1 << 7)) >> 7 // get the highest-order bit
 	b.Bits[0] = b.Bits[0] * 2
-	b.NumBits = b.NumBits + 1
+	b.NumBits++
 	if b.NumBits == 8 {
 		// we need the next byte!
-		n, err := b.File.Read(b.Bits)
-		if err != nil {
-			return 0, err
-		}
-		if n == 0 {
-			return 0, errors.New("Couldn't read from file")
-		}
-		b.NumBits = 0
+		b.nextByte()
 	}
 	return bit, nil
 }
@@ -45,4 +42,16 @@ func (b BitReader) ReadBit() (bit byte, err error) {
 // Closes the reader, closing its associated file descriptor
 func (b BitReader) Close() (err error) {
 	return b.File.Close()
+}
+
+func (b BitReader) nextByte() (err error) {
+	n, err := b.File.Read(b.Bits)
+	if err != nil {
+		return err
+	}
+	if n == 0 {
+		return errors.New("Couldn't read from file")
+	}
+	b.NumBits = 0
+	return nil
 }

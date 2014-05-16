876d9700ea7ee6b17cb4eac44f9544ee31b1412f
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 12:38:44 -0700

    fixed multi-byte EOF problem on bitreader

--- bitReader.go
+++ bitReader.go
@@ -28,23 +28,23 @@ func NewReader(file string) (b BitReader, err error) {
 
 // Returns the next bit on the file stream. Will always be 0 or 1. Will
 // return a non-nil err iff the read failed, or on EOF
-func (b BitReader) ReadBit() (bit byte, err error) {
+func (b *BitReader) ReadBit() (bit byte, err error) {
+	if b.NumBits == 8 {
+		// we need the next byte!
+		err = b.nextByte()
+	}
 	bit = (b.Bits[0] & (1 << 7)) >> 7 // get the highest-order bit
 	b.Bits[0] = b.Bits[0] * 2         // get rid of the highest-order bit
 	b.NumBits++
-	if b.NumBits == 8 {
-		// we need the next byte!
-		b.nextByte()
-	}
-	return bit, nil
+	return bit, err
 }
 
 // Closes the reader, closing its associated file descriptor
-func (b BitReader) Close() (err error) {
+func (b *BitReader) Close() (err error) {
 	return b.File.Close()
 }
 
-func (b BitReader) nextByte() (err error) {
+func (b *BitReader) nextByte() (err error) {
 	n, err := b.File.Read(b.Bits)
 	if err != nil {
 		return err

3367c287f6a18ea206c43a1690a18d59a6ff153e
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 13:38:45 -0700

    more bitreader tests

--- bitReader_test.go
+++ bitReader_test.go
@@ -71,7 +71,8 @@ func TestBasicFileRead(t *testing.T) {
 }
 
 func TestFileReadOffEndOfFile(t *testing.T) {
-	bytes := make([]byte, 1)
+	numBytes := 4
+	bytes := make([]byte, numBytes)
 	err := ioutil.WriteFile(filename, bytes, 0644)
 	defer os.Remove(filename)
 	if err != nil {
@@ -84,7 +85,7 @@ func TestFileReadOffEndOfFile(t *testing.T) {
 	}
 
 	// get rid of those 8 bits
-	for i := 0; i < 8; i++ {
+	for i := 0; i < 8*numBytes; i++ {
 		_, err := br.ReadBit()
 		if err != nil {
 			// Something went wrong before we even got to what we're testing
@@ -99,7 +100,57 @@ func TestFileReadOffEndOfFile(t *testing.T) {
 	}
 }
 
-func TestMultiByteFileRead(t *testing.T) {
+func TestEOFFirstReadOfEmptyFile(t *testing.T) {
+	err := ioutil.WriteFile(filename, make([]byte, 0), 0644)
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	br, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Now we should get an error
+	_, err = br.ReadBit()
+	if err == nil {
+		t.Error("Should have gotten EOF error, got nil instead")
+	}
+}
+
+func TestMultiSameBytesFileRead(t *testing.T) {
+	numBytes := 4
+	bytes := make([]byte, numBytes)
+	for i := 0; i < numBytes; i++ {
+		bytes[i] = 255 // all 1s
+	}
+
+	err := ioutil.WriteFile(filename, bytes, 0644)
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	br, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// We should get all 1s
+	for i := 0; i < 8*numBytes; i++ {
+		bit, err := br.ReadBit()
+		if err != nil {
+			t.Error(err)
+		}
+
+		if bit != 1 {
+			t.Error("Wanted 1, got:", bit)
+		}
+	}
+}
+
+func TestMultiDifferentBytesFileRead(t *testing.T) {
 	numBytes := 4
 	bytes := make([]byte, numBytes)
 	for i := 0; i < numBytes; i++ {

e95ef6e99c940507b17f183b59f3c2eaae21a272
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 12:18:19 -0700

    updated test

--- bitReader_test.go
+++ bitReader_test.go
@@ -19,12 +19,15 @@ var filename = ".test"
 ////////////////////////////////////////////////////////////////////////////////
 
 func TestNewReader(t *testing.T) {
-	err := makeBasicFile()
+	err := ioutil.WriteFile(filename, make([]byte, 1), 0644)
 	defer os.Remove(filename)
 	if err != nil {
 		t.Error(err)
 	}
 	b, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
 	b.Close()
 }
 
@@ -40,8 +43,12 @@ func TestNewReaderReturnsErrorOnNonExistentFile(t *testing.T) {
 ////////////////////////////////////////////////////////////////////////////////
 
 func TestBasicFileRead(t *testing.T) {
-	err := makeBasicFile()
-	//defer os.Remove(filename)
+
+	bytes := make([]byte, 1)
+	bytes[0] = 255 // all 1s
+
+	err := ioutil.WriteFile(filename, bytes, 0644)
+	defer os.Remove(filename)
 	if err != nil {
 		t.Error(err)
 	}
@@ -51,38 +58,15 @@ func TestBasicFileRead(t *testing.T) {
 		t.Error(err)
 	}
 
-	// We should get 7 0's
+	// We should get all 1s
 	for i := 0; i < 8; i++ {
 		bit, err := br.ReadBit()
 		if err != nil {
 			t.Error(err)
 		}
-		if bit != 0 {
-			t.Error("Got incorrect bit. Should be 0, got", bit, "for bit", i)
+
+		if bit != 1 {
+			t.Error("Wanted 1, got:", bit)
 		}
 	}
-
-	// Then a 1
-	bit, err := br.ReadBit()
-	if err != nil {
-		t.Error(err)
-	}
-	if bit != 1 {
-		t.Error("Got incorrect bit. Should be 1, got", bit)
-	}
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Helper functions
-////////////////////////////////////////////////////////////////////////////////
-
-// Makes a basic file to be used by the test suite. File bytes will be
-// ascending powers of two.
-func makeBasicFile() (err error) {
-	bytes := make([]byte, 9)
-	for i := 0; i < len(bytes)-1; i++ {
-		bytes[i] = 1 << uint(i)
-	}
-	err = ioutil.WriteFile(filename, bytes, 0644)
-	return err
 }

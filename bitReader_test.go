cce85d81da73120b0e08c6adf97b0b0b67131ebe
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 12:25:24 -0700

    wrote another test

--- bitReader_test.go
+++ bitReader_test.go
@@ -43,8 +43,65 @@ func TestNewReaderReturnsErrorOnNonExistentFile(t *testing.T) {
 ////////////////////////////////////////////////////////////////////////////////
 
 func TestBasicFileRead(t *testing.T) {
-
 	bytes := make([]byte, 1)
+	bytes[0] = 255 // all 1s
+
+	err := ioutil.WriteFile(filename, bytes, 0644)
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	br, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// We should get all 1s
+	for i := 0; i < 8; i++ {
+		bit, err := br.ReadBit()
+		if err != nil {
+			t.Error(err)
+		}
+
+		if bit != 1 {
+			t.Error("Wanted 1, got:", bit)
+		}
+	}
+}
+
+func TestFileReadOffEndOfFile(t *testing.T) {
+	bytes := make([]byte, 1)
+
+	err := ioutil.WriteFile(filename, bytes, 0644)
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	br, err := NewReader(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// get rid of those 8 bits
+	for i := 0; i < 1000; i++ {
+		_, err := br.ReadBit()
+		if err != nil {
+			// Something went wrong before we even got to what we're testing
+			t.Error(err)
+		}
+	}
+
+	// Now we should get an error
+	_, err = br.ReadBit()
+	if err == nil {
+		t.Error("Should have gotten EOF error, got nil instead")
+	}
+}
+
+func TestMultiByteFileRead(t *testing.T) {
+	bytes := make([]byte, 4)
 	bytes[0] = 255 // all 1s
 
 	err := ioutil.WriteFile(filename, bytes, 0644)

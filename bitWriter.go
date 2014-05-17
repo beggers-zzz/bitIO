81a8cb93af7a726899c7d1006ab3280ae3f8e677
Ben Eggers <beggerss@cs.washington.edu>
2014-05-16 20:29:32 -0700

    made bitwriter methods act on pointers

--- bitWriter.go
+++ bitWriter.go
@@ -27,7 +27,7 @@ func NewWriter(file string) (b BitWriter, err error) {
 
 // Writes one bit. If the passed int8 is 1, writes a one. If it's 0,
 // writes a 0. Else, returns a non-nil error.
-func (b BitWriter) WriteBit(bit byte) (err error) {
+func (b *BitWriter) WriteBit(bit byte) (err error) {
 	if bit != 0 && bit != 1 {
 		return errors.New("Invalid bit to write.")
 	}
@@ -45,7 +45,7 @@ func (b BitWriter) WriteBit(bit byte) (err error) {
 }
 
 // Flushes the current byte out to disk, padding with 0s if necessary.
-func (b BitWriter) flush() (err error) {
+func (b *BitWriter) flush() (err error) {
 	for b.NumBits != 8 {
 		b.WriteBit(0)
 	}
@@ -55,7 +55,7 @@ func (b BitWriter) flush() (err error) {
 
 // Closes the BitReader, flushing final bits to disk if need be and closing
 // the file descriptor.
-func (b BitWriter) Close() (err error) {
+func (b *BitWriter) Close() (err error) {
 	err = b.flush()
 	if err != nil {
 		return err

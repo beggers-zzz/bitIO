d4f748ff96c6789c28f196da4f4fdadb8abbab38
Ben Eggers <beggerss@cs.washington.edu>
2014-05-19 00:49:29 -0700

    another bitwriter test

--- bitWriter_test.go
+++ bitWriter_test.go
@@ -132,3 +132,54 @@ func TestWriteMultipleSameBytes(t *testing.T) {
 	}
 }
 
+func TestWriteMultipleDifferentBytes(t *testing.T) {
+	bw, err := NewWriter(filename)
+	defer os.Remove(filename)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Let's write some bytes! First 128
+	bw.WriteBit(1)
+	for i := 0; i < 7; i++ {
+		bw.WriteBit(0)
+	}
+
+	// Then 0
+	for i := 0; i < 8; i++ {
+		bw.WriteBit(0)
+	}
+
+	// Then 170
+	for i := 0; i < 4; i++ {
+		bw.WriteBit(1)
+		bw.WriteBit(0)
+	}
+
+	// And close to make sure they're written out
+	bw.Close()
+
+	// Now let's check the file is there there
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Something went badly on opening? Weird
+		t.Error(err)
+	}
+
+	if len(b) != 3 {
+		// We read more bytes than we should have, error
+		t.Error("Got a different number of bytes than we should have:", len(b))
+	}
+
+	if b[0] != 128 {
+		t.Error("Should have been all 0b1000000 (128), got:", b[0])
+	}
+
+		if b[1] != 0 {
+		t.Error("Should have been all 0s, got:", b[1])
+	}
+
+		if b[2] != 170 {
+		t.Error("Should have been all 0b10101010 (170), got:", b[2])
+	}
+}
